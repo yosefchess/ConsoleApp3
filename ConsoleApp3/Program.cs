0cc003bd6be097b019de2fbe89115aa70287ddab
IT Shop <IT Shop@DESKTOP-BIH0M27>
2021-11-27 00:40:10 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("it isn't a prime number");
+                    return;
+                }
+            }
+            Console.WriteLine("it is a prime number");
+
+        }
+    }
+}
